6ed5f2ff5ddb6341e33c1f181dcf429b8f509be8
Peter <peter@peterk.co>
2020-02-09 21:59:02 +0000

    docker posix make config

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,6 @@ RUN curl ${download_url} -o nRF5_SDK.zip \
   && rm nRF5_SDK.zip
 
 COPY entrypoint.sh /entrypoint.sh
+COPY Makefile.posix /nrf-sdk/components/toolchain/gcc/Makefile.posix
 
 CMD [ "/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,5 @@ export GNU_INSTALL_ROOT=${GNUARMEMB_TOOLCHAIN_PATH}
 export GNU_VERSION=9.2.1
 export GNU_PREFIX=arm-none-eabi
 
-sleep infinity
+cd /nrf-sdk/examples/ble_peripheral/ble_app_blinky/pca10059/s140/
+make -j $(nproc)
